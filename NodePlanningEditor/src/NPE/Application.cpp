242c633c5bc3b12b91ed7e92ff26afa280677a8f
Simon <simon_geier@web.de>
2020-09-07 19:59:12 +0200

    Reverted event test

--- NodePlanningEditor/src/NPE/Application.cpp
+++ NodePlanningEditor/src/NPE/Application.cpp
@@ -3,21 +3,14 @@
 
 #include "GUI/Events/ApplicationEvent.h"
 
-#include "GUI/Controls/MenuControl.h"
 
 namespace NPE
 {
-	GUI::MenuItem* item;
 	Application::Application()
 		: m_FileHandler("saves\\save.dbs")
 	{
 		m_FileHandler.LoadScene(this->GetWindow());
 		InstallEventFilter([this](GUI::Control* watched, GUI::Event& e) { return OnEvent(watched, e); });
-
-		GUI::MenuBar* mb = (GUI::MenuBar*)m_Window.AddControl(new GUI::MenuBar(&m_Window));
-		
-		item = new GUI::MenuItem(&m_Window, "Hello");
-		mb->AddItem(item);
 	}
 
 	bool Application::OnEvent(GUI::Control* watched, GUI::Event& e)
@@ -27,11 +20,6 @@ namespace NPE
 		case GUI::EventType::AppPaintEvent:
 		{
 			return OnPaintEvent((GUI::PaintEvent&)e);
-		}
-		case GUI::EventType::MouseButtonPressedEvent:
-		{
-			if (watched == item)
-				MessageBox(NULL, L"CLICKED", L"", NULL);
 		}
 		}
 		return false;
