5802f190f8d587a5c4ce7c914f8b85a39958c9a1
Simon <simon_geier@web.de>
2020-09-11 22:15:46 +0200

    Added more buttons to text edit window

--- NPEGUI/src/GUI/Controls/HWNDTextBox.cpp
+++ NPEGUI/src/GUI/Controls/HWNDTextBox.cpp
@@ -17,17 +17,23 @@ namespace GUI
 
 	void HWNDTextBox::CreateTextWindow(unsigned int x, unsigned int y, unsigned int width, unsigned int height)
 	{
-		if (!CreateNativeWindow(L"", WS_OVERLAPPEDWINDOW, 0, x, y, width, height))
+		if (!CreateNativeWindow(L"", WS_SYSMENU, 0, x, y, width, height))
 		{
 			NPE_THROW_WND_EXCEPT(GetLastError());
 		}
 
-		m_hWndEdit = CreateWindowEx(0, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL, 0, 0, width, height, m_hWnd, 0, 0, 0);
+		m_hWndEdit = CreateWindowEx(0, L"EDIT", L"", WS_BORDER | WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL, 20, 20, width - 40, height - 120, m_hWnd, 0, 0, 0);
 
 		if (!m_hWndEdit)
-		{
 			NPE_THROW_WND_EXCEPT(GetLastError());
-		}
+
+		m_hWndOkBtn = CreateWindowEx(0, L"BUTTON", L"Ok", WS_TABSTOP | BS_DEFPUSHBUTTON | WS_BORDER | WS_CHILD | WS_VISIBLE, 20, height - 90, 150, 40, m_hWnd, (HMENU)1, GetModuleHandle(NULL), 0);
+
+		if (!m_hWndOkBtn)
+			NPE_THROW_WND_EXCEPT(GetLastError());
+
+		m_HWndCancelBtn = CreateWindowEx(0, L"BUTTON", L"Cancel", WS_TABSTOP | BS_DEFPUSHBUTTON | WS_BORDER | WS_CHILD | WS_VISIBLE, 190, height - 90, 150, 40, m_hWnd, (HMENU)2, GetModuleHandle(NULL), 0);
+
 		ShowWindow(m_hWnd, SW_SHOWDEFAULT);
 	}
 
@@ -35,13 +41,23 @@ namespace GUI
 	{
 		switch (uMsg)
 		{
+		case WM_CREATE:
+		{
+			// Disable default titlebar icon 
+			int extendedStyle = GetWindowLong(m_hWnd, GWL_EXSTYLE);
+			SetWindowLong(m_hWnd, GWL_EXSTYLE, extendedStyle | WS_EX_DLGMODALFRAME);
+
+			// Update non-client area of the dialog, for the changes to take effect
+			SetWindowPos(NULL, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER, 0);
+			return 0;
+		}
 		case WM_DESTROY:
 		{
 			int len = GetWindowTextLength(m_hWndEdit);
-			if(len < m_Text.capacity())
-				m_Text.reserve(len + 1);
+			if(len < m_Parent->m_Text.capacity())
+				m_Parent->m_Text.reserve(len + 1);
 			
-			GetWindowText(m_hWndEdit, (wchar_t*)m_Parent->m_Text.c_str(), (int)m_Text.capacity());
+			GetWindowText(m_hWndEdit, (wchar_t*)m_Parent->m_Text.c_str(), (int)m_Parent->m_Text.capacity());
 
 			Renderer::Get().BeginDraw();
 			
--- NPEGUI/src/GUI/Controls/HWNDTextBox.h
+++ NPEGUI/src/GUI/Controls/HWNDTextBox.h
@@ -14,15 +14,13 @@ namespace GUI
 
 		void CreateTextWindow(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
 
-		const std::wstring& GetText() const { return m_Text; }
-
-		void FlushText() { m_Text = L""; }
-
 		virtual LRESULT CALLBACK HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 	private:
 		HWND m_hWndEdit;
-		std::wstring m_Text;
+		HWND m_hWndOkBtn;
+		HWND m_HWndCancelBtn;
+
 		TextBox* m_Parent;
 	};
 
