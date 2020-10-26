0d9676f72574c14135562fd7cfcaf1194c59f5ff
Simon <simon_geier@web.de>
2020-10-26 10:03:05 +0100

    Fixed log file not being closed

--- Reyal-GUI/src/Reyal/EntryPoint.h
+++ Reyal-GUI/src/Reyal/EntryPoint.h
@@ -71,6 +71,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR p
 #ifdef _DEBUG
 		FreeConsole();
 #endif
+		ZL_LOG_END();
 		return exitCode;
 	}
 	// TODO: Custom Window for errors
