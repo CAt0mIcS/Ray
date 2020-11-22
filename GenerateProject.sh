b396dd6d7a026d630985572b382acddd75883453
Simon <simon_geier@web.de>
2020-11-22 18:33:44 +0100

    Removed unnecessary flags in linux script

--- GenerateProject.sh
+++ GenerateProject.sh
@@ -25,7 +25,7 @@ echo Selecting Build Settings "$configuration" "$unameOut" and "$architecture"
 
 [ ! -d "./$buildPath" ] && mkdir ./$buildPath
 cd ./$buildPath
-cmake .. -DCMAKE_BUILD_TYPE=$configuration -DRL_ARCHITECTURE=$architecture -DGLFW_INSTALL=0 -DGLFW_BUILD_DOCS=0
+cmake .. -DCMAKE_BUILD_TYPE=$configuration -DRL_ARCHITECTURE=$architecture
 
 read -s -n 1 -p "Press any key to continue . . ."
 cd ..
