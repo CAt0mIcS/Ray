281db0d6154c9ff9ed1242d559d550e219e6aa3a
Simon <simon_geier@web.de>
2020-12-06 14:44:46 +0100

    Fixed Z-Buffer occlusion not working

--- src/Reyal/RlRender/include/RlRender/Renderer3D.cpp
+++ src/Reyal/RlRender/include/RlRender/Renderer3D.cpp
@@ -21,7 +21,7 @@ namespace At0::Reyal
         m_hWnd = hWnd;
         
         RECT clientWindowRect;
-        GetClientRect(m_hWnd, &clientWindowRect);
+        RL_THROW_WND_EXCEPT(GetClientRect(m_hWnd, &clientWindowRect));
 
 
         // ----------------------------------------------------------------------------------------------------
@@ -29,8 +29,8 @@ namespace At0::Reyal
         DXGI_SWAP_CHAIN_DESC sd{};
 
         // Let DXGI figure it out with hWnd
-        sd.BufferDesc.Width = 0;
-        sd.BufferDesc.Height = 0;
+        sd.BufferDesc.Width = clientWindowRect.right;
+        sd.BufferDesc.Height = clientWindowRect.bottom;
 
         sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
         sd.BufferDesc.RefreshRate = { 0, 0 };
@@ -49,61 +49,67 @@ namespace At0::Reyal
 
         RL_GFX_THROW_FAILED(s_pIDXGIFactory->CreateSwapChain(s_pDevice, &sd, &m_pSwapChain));
 
-        // ----------------------------------------------------------------------------------------------------
-        // Back Buffer
-        WRL::ComPtr<ID3D11Resource> pBackBuffer;
-        m_pSwapChain->GetBuffer(0u, __uuidof(ID3D11Resource), &pBackBuffer);
 
+        // ----------------------------------------------------------------------------------------------------
+        // gain access to texture subresource in swap chain (back buffer)
+        WRL::ComPtr<ID3D11Resource> pBackBuffer;
+        RL_GFX_THROW_FAILED(m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer));
         RL_GFX_THROW_FAILED(s_pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &m_pTarget));
 
         // ----------------------------------------------------------------------------------------------------
-        // Depth Stencil state
-        D3D11_DEPTH_STENCIL_DESC dsDesc{};
+        // create depth stensil state
+        D3D11_DEPTH_STENCIL_DESC dsDesc = {};
         dsDesc.DepthEnable = TRUE;
         dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
         dsDesc.DepthFunc = D3D11_COMPARISON_LESS;
         WRL::ComPtr<ID3D11DepthStencilState> pDSState;
         RL_GFX_THROW_FAILED(s_pDevice->CreateDepthStencilState(&dsDesc, &pDSState));
-        s_pContext->OMSetDepthStencilState(pDSState.Get(), 1);
 
         // ----------------------------------------------------------------------------------------------------
-        // Create Depth Stencil Texture
+        // bind depth state
+        s_pContext->OMSetDepthStencilState(pDSState.Get(), 1u);
+
+        // ----------------------------------------------------------------------------------------------------
+        // create depth stensil texture
         WRL::ComPtr<ID3D11Texture2D> pDepthStencil;
-        D3D11_TEXTURE2D_DESC descDepth{};
+        D3D11_TEXTURE2D_DESC descDepth = {};
         descDepth.Width = clientWindowRect.right;
         descDepth.Height = clientWindowRect.bottom;
-        descDepth.MipLevels = 1;
-        descDepth.ArraySize = 1;
+        descDepth.MipLevels = 1u;
+        descDepth.ArraySize = 1u;
         descDepth.Format = DXGI_FORMAT_D32_FLOAT;
-        descDepth.SampleDesc.Count = 1;
-        descDepth.SampleDesc.Quality = 0;
+        descDepth.SampleDesc.Count = 1u;
+        descDepth.SampleDesc.Quality = 0u;
         descDepth.Usage = D3D11_USAGE_DEFAULT;
         descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
         RL_GFX_THROW_FAILED(s_pDevice->CreateTexture2D(&descDepth, nullptr, &pDepthStencil));
 
         // ----------------------------------------------------------------------------------------------------
-        // Create View of Depth Stencil Texture
-        D3D11_DEPTH_STENCIL_VIEW_DESC descDSV{};
+        // create view of depth stensil texture
+        D3D11_DEPTH_STENCIL_VIEW_DESC descDSV = {};
         descDSV.Format = DXGI_FORMAT_D32_FLOAT;
         descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
-        descDSV.Texture2D.MipSlice = 0;
-        RL_GFX_THROW_FAILED(s_pDevice->CreateDepthStencilView(
-            pDepthStencil.Get(), &descDSV, &m_pDSV
-        ));
-        s_pContext->OMSetRenderTargets(1, m_pTarget.GetAddressOf(), m_pDSV.Get());
+        descDSV.Texture2D.MipSlice = 0u;
+        RL_GFX_THROW_FAILED(s_pDevice->CreateDepthStencilView(pDepthStencil.Get(), &descDSV, &m_pDSV));
+
+        // ----------------------------------------------------------------------------------------------------
+        // bind depth stensil view to OM
+        s_pContext->OMSetRenderTargets(1u, m_pTarget.GetAddressOf(), m_pDSV.Get());
+
+        // ----------------------------------------------------------------------------------------------------
+        // configure viewport
+        D3D11_VIEWPORT vp;
+        vp.Width = clientWindowRect.right;
+        vp.Height = clientWindowRect.bottom;
+        vp.MinDepth = 0.0f;
+        vp.MaxDepth = 1.0f;
+        vp.TopLeftX = 0.0f;
+        vp.TopLeftY = 0.0f;
+        s_pContext->RSSetViewports(1u, &vp);
     }
     
     void Renderer3D::DrawIndexed(uint32_t indicesCount)
     {
-        // TODO: This needs to be done here when drawing in multiple windows (Some of it is in the constructor (duplicates))
-        RECT rc;
-        GetClientRect(m_hWnd, &rc);
-        D3D11_VIEWPORT vp{};
-        vp.Width = rc.right;
-        vp.Height = rc.bottom;
-        s_pContext->RSSetViewports(1, &vp);
-        s_pContext->OMSetRenderTargets(1, m_pTarget.GetAddressOf(), m_pDSV.Get());
-
         s_pContext->DrawIndexed(indicesCount, 0, 0);
     }
 
@@ -111,7 +117,7 @@ namespace At0::Reyal
     {
         const float color[] = { red,green,blue,1.0f };
         s_pContext->ClearRenderTargetView(m_pTarget.Get(), color);
-        s_pContext->ClearDepthStencilView(m_pDSV.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0);
+        s_pContext->ClearDepthStencilView(m_pDSV.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0u);
     }
 
     void Renderer3D::EndDraw()
