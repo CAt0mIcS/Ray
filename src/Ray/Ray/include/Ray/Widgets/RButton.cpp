9a922fded67fdd59286753605bc9319e19558281
Simon <simon_geier@web.de>
2020-12-26 15:38:10 +0100

    Possible button implementation

--- src/Ray/Ray/include/Ray/Widgets/RButton.cpp
+++ src/Ray/Ray/include/Ray/Widgets/RButton.cpp
@@ -3,5 +3,30 @@
 
 namespace At0::Ray
 {
+	Button::Button(const std::string_view name, Widget& parent)
+		: Widget(name, &parent)
+	{
 
+	}
+
+	void Button::AddPaintEventListener(EventDispatcher<PaintEvent>& dispatcher)
+	{
+		EventListener<PaintEvent>::Subscribe(dispatcher);
+	}
+
+	void Button::SetPos(const Point2& pos)
+	{
+	}
+
+	void Button::SetSize(const Size2& size)
+	{
+	}
+
+	Button::~Button()
+	{
+	}
+
+	void Button::OnEvent(Widget* receiver, PaintEvent& e)
+	{
+	}
 }
--- src/Ray/Ray/include/Ray/Widgets/RButton.h
+++ src/Ray/Ray/include/Ray/Widgets/RButton.h
@@ -3,11 +3,27 @@
 #include "../RBase.h"
 #include "RWidget.h"
 
+#include "../../RayEvent/include/RayEvent/RayEvent.h"
+
 
 namespace At0::Ray
 {
-	class RAY_API Button : public Widget
+	class RAY_API Button : public Widget,
+		EventListener<PaintEvent>
 	{
+	public:
+		Button(const std::string_view name, Widget& parent);
+		void AddPaintEventListener(EventDispatcher<PaintEvent>& dispatcher);
+
+		virtual void SetPos(const Point2& pos) override;
+		virtual void SetSize(const Size2& size) override;
+
+		~Button();
+
+	private:
+		virtual void OnEvent(Widget* receiver, PaintEvent& e) override;
+
+	private:
 
 	};
 }
