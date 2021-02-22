6e41772f44c45deac46cf61815e2e79199e67c8b
CAt0mIcS <simon_geier@web.de>
2021-02-22 11:39:15 +0100

    Converted asserts to exceptions

--- Sources/Graphics/Pipelines/RUniformAccess.cpp
+++ Sources/Graphics/Pipelines/RUniformAccess.cpp
@@ -5,7 +5,7 @@
 #include "RPipeline.h"
 
 #include "Utils/RString.h"
-#include "Utils/RAssert.h"
+#include "Utils/RException.h"
 
 
 namespace At0::Ray
@@ -19,28 +19,32 @@ namespace At0::Ray
 		if (uniformBlockName.empty())
 		{
 			auto uniforms = m_Shader.GetUniforms(stageFlag);
-			RAY_MEXPECTS(uniforms, "[UniformAccess] No uniforms registered for shader stage {0}.",
-				String::Construct(stageFlag));
+			if (!uniforms)
+				RAY_THROW_RUNTIME("[UniformAccess] No uniforms registered for shader stage {0}.",
+					String::Construct(stageFlag));
 
 			auto uniformData = uniforms->Get(uniformName);
-			RAY_MEXPECTS(uniformData, "[UniformAccess] Uniform {0} was not found.", uniformName);
+			if (!uniformData)
+				RAY_THROW_RUNTIME("[UniformAccess] Uniform {0} was not found.", uniformName);
 
 			return { uniformData->offset };
 		}
 		else
 		{
 			auto uniformBlocks = m_Shader.GetUniformBlocks(stageFlag);
-			RAY_MEXPECTS(uniformBlocks,
-				"[UniformAccess] No uniform blocks registered for shader stage {0}.",
-				String::Construct(stageFlag));
+			if (!uniformBlocks)
+				RAY_THROW_RUNTIME(
+					"[UniformAccess] No uniform blocks registered for shader stage {0}.",
+					String::Construct(stageFlag));
 
 			auto uniformBlockData = uniformBlocks->Get(uniformBlockName);
-			RAY_MEXPECTS(uniformBlockData, "[UniformAccess] Uniform Block {0} was not found.",
-				uniformBlockName);
+			if (!uniformBlockData)
+				RAY_THROW_RUNTIME(
+					"[UniformAccess] Uniform Block {0} was not found.", uniformBlockName);
 
 			auto uniformData = uniformBlockData->uniforms.Get(uniformName);
-			RAY_MEXPECTS(
-				uniformBlockData, "[UniformAccess] Uniform {0} was not found.", uniformName);
+			if (!uniformBlockData)
+				RAY_THROW_RUNTIME("[UniformAccess] Uniform {0} was not found.", uniformName);
 
 			return { uniformData->offset };
 		}
