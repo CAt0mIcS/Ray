58be485e61c99c3adccbfd332a34095311adbe7e
Simon <simon_geier@web.de>
2021-06-01 09:39:26 +0200

    Console-/File-only loggers

--- Sources/Utils/RLogger.cpp
+++ Sources/Utils/RLogger.cpp
@@ -4,26 +4,32 @@
 
 namespace At0::Ray
 {
-	Violent::FileLogger Log::s_FileLogger;
-	Violent::ConsoleLogger Log::s_ConsoleLogger;
+	void CLog::Close() { g_ConsoleLogger.Close(); }
+	void CLog::SetLogLevel(Violent::LogLevel lvl) { g_ConsoleLogger.SetLogLevel(lvl); }
+	void CLog::Flush() { g_ConsoleLogger.Flush(); }
 
-	void Log::Open(const char* filepath) { s_FileLogger.Open(filepath); }
+	void FLog::Open(const char* filepath) { g_FileLogger.Open(filepath); }
+	void FLog::Close() { g_FileLogger.Close(); }
+	void FLog::SetLogLevel(Violent::LogLevel lvl) { g_FileLogger.SetLogLevel(lvl); }
+	void FLog::Flush() { g_FileLogger.Flush(); }
+
+	void Log::Open(const char* filepath) { FLog::Open(filepath); }
 
 	void Log::Close()
 	{
-		s_FileLogger.Close();
-		s_ConsoleLogger.Close();
+		FLog::Close();
+		CLog::Close();
 	}
 
 	void Log::SetLogLevel(Violent::LogLevel lvl)
 	{
-		s_FileLogger.SetLogLevel(lvl);
-		s_ConsoleLogger.SetLogLevel(lvl);
+		FLog::SetLogLevel(lvl);
+		CLog::SetLogLevel(lvl);
 	}
 
 	void Log::Flush()
 	{
-		s_FileLogger.Flush();
-		s_ConsoleLogger.Flush();
+		FLog::Flush();
+		CLog::Flush();
 	}
 }  // namespace At0::Ray
--- Sources/Utils/RLogger.h
+++ Sources/Utils/RLogger.h
@@ -11,10 +11,14 @@
 
 namespace At0::Ray
 {
-	class RAY_EXPORT Log
+	inline Violent::FileLogger g_FileLogger;
+	inline Violent::ConsoleLogger g_ConsoleLogger;
+
+	class RAY_EXPORT CLog
 	{
+		friend class Log;
+
 	public:
-		static void Open(const char* filepath);
 		static void Close();
 		static void SetLogLevel(Violent::LogLevel lvl);
 		static void Flush();
@@ -22,66 +26,60 @@ namespace At0::Ray
 		template<typename... Args>
 		struct Trace
 		{
-			Trace(std::string_view msg, Args&&... args,
+			Trace(std::string_view Cmsg, Args&&... args,
 				std::source_location loc = std::source_location::current())
 			{
-				s_FileLogger.Trace(msg, std::forward<Args>(args)...);
-				s_ConsoleLogger.Trace(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Trace(Cmsg, std::forward<Args>(args)...);
 			}
 		};
 
 		template<typename... Args>
 		struct Debug
 		{
-			Debug(std::string_view msg, Args&&... args,
+			Debug(std::string_view Cmsg, Args&&... args,
 				std::source_location loc = std::source_location::current())
 			{
-				s_FileLogger.Debug(msg, std::forward<Args>(args)...);
-				s_ConsoleLogger.Debug(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Debug(Cmsg, std::forward<Args>(args)...);
 			}
 		};
 
 		template<typename... Args>
 		struct Info
 		{
-			Info(std::string_view msg, Args&&... args,
+			Info(std::string_view Cmsg, Args&&... args,
 				std::source_location loc = std::source_location::current())
 			{
-				s_FileLogger.Info(msg, std::forward<Args>(args)...);
-				s_ConsoleLogger.Info(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Info(Cmsg, std::forward<Args>(args)...);
 			}
 		};
 
 		template<typename... Args>
 		struct Warn
 		{
-			Warn(std::string_view msg, Args&&... args,
+			Warn(std::string_view Cmsg, Args&&... args,
 				std::source_location loc = std::source_location::current())
 			{
-				s_FileLogger.Warn(msg, std::forward<Args>(args)...);
-				s_ConsoleLogger.Warn(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Warn(Cmsg, std::forward<Args>(args)...);
 			}
 		};
 
 		template<typename... Args>
 		struct Error
 		{
-			Error(std::string_view msg, Args&&... args,
+			Error(std::string_view Cmsg, Args&&... args,
 				std::source_location loc = std::source_location::current())
 			{
-				s_FileLogger.Error(msg, std::forward<Args>(args)...);
-				s_ConsoleLogger.Error(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Error(Cmsg, std::forward<Args>(args)...);
 			}
 		};
 
 		template<typename... Args>
 		struct Critical
 		{
-			Critical(std::string_view msg, Args&&... args,
+			Critical(std::string_view Cmsg, Args&&... args,
 				std::source_location loc = std::source_location::current())
 			{
-				s_FileLogger.Critical(msg, std::forward<Args>(args)...);
-				s_ConsoleLogger.Critical(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Critical(Cmsg, std::forward<Args>(args)...);
 			}
 		};
 
@@ -102,9 +100,187 @@ namespace At0::Ray
 
 		template<typename... Args>
 		Critical(std::string_view, Args&&...) -> Critical<Args...>;
+	};
 
-	private:
-		static Violent::FileLogger s_FileLogger;
-		static Violent::ConsoleLogger s_ConsoleLogger;
+	class RAY_EXPORT FLog
+	{
+		friend class Log;
+
+	public:
+		static void Open(const char* filepath);
+		static void Close();
+		static void SetLogLevel(Violent::LogLevel lvl);
+		static void Flush();
+
+		template<typename... Args>
+		struct Trace
+		{
+			Trace(std::string_view Fmsg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Trace(Fmsg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Debug
+		{
+			Debug(std::string_view Fmsg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Debug(Fmsg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Info
+		{
+			Info(std::string_view Fmsg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Info(Fmsg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Warn
+		{
+			Warn(std::string_view Fmsg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Warn(Fmsg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Error
+		{
+			Error(std::string_view Fmsg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Error(Fmsg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Critical
+		{
+			Critical(std::string_view Fmsg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Critical(Fmsg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		Trace(std::string_view, Args&&...) -> Trace<Args...>;
+
+		template<typename... Args>
+		Debug(std::string_view, Args&&...) -> Debug<Args...>;
+
+		template<typename... Args>
+		Info(std::string_view, Args&&...) -> Info<Args...>;
+
+		template<typename... Args>
+		Warn(std::string_view, Args&&...) -> Warn<Args...>;
+
+		template<typename... Args>
+		Error(std::string_view, Args&&...) -> Error<Args...>;
+
+		template<typename... Args>
+		Critical(std::string_view, Args&&...) -> Critical<Args...>;
+	};
+
+	class RAY_EXPORT Log
+	{
+	public:
+		static void Open(const char* filepath);
+		static void Close();
+		static void SetLogLevel(Violent::LogLevel lvl);
+		static void Flush();
+
+		template<typename... Args>
+		struct Trace
+		{
+			Trace(std::string_view msg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Trace(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Trace(msg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Debug
+		{
+			Debug(std::string_view msg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Debug(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Debug(msg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Info
+		{
+			Info(std::string_view msg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Info(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Info(msg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Warn
+		{
+			Warn(std::string_view msg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Warn(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Warn(msg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Error
+		{
+			Error(std::string_view msg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Error(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Error(msg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		struct Critical
+		{
+			Critical(std::string_view msg, Args&&... args,
+				std::source_location loc = std::source_location::current())
+			{
+				g_FileLogger.Critical(msg, std::forward<Args>(args)...);
+				g_ConsoleLogger.Critical(msg, std::forward<Args>(args)...);
+			}
+		};
+
+		template<typename... Args>
+		Trace(std::string_view, Args&&...) -> Trace<Args...>;
+
+		template<typename... Args>
+		Debug(std::string_view, Args&&...) -> Debug<Args...>;
+
+		template<typename... Args>
+		Info(std::string_view, Args&&...) -> Info<Args...>;
+
+		template<typename... Args>
+		Warn(std::string_view, Args&&...) -> Warn<Args...>;
+
+		template<typename... Args>
+		Error(std::string_view, Args&&...) -> Error<Args...>;
+
+		template<typename... Args>
+		Critical(std::string_view, Args&&...) -> Critical<Args...>;
 	};
 }  // namespace At0::Ray
